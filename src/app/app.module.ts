48147737a297d7e82d0e3caf7fd4cf283d69a3b0
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-01-07 23:48:43 +0530

    Directives: custom directives and accessing and manupilating the elements in DOM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { basicHighLightDirectives } from './basic-highlights/basic-highlights.directives';
+import { basicHighLightDirectives } from './basic-highlights/basic-highlights.directive';
+import { BetterHighlightDirective } from './better-highlights/better-highlights.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    basicHighLightDirectives
+    basicHighLightDirectives,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
